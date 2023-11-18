98e93b8a5471aa470dd6206d59d861069c478694
Laeth Nueirat <l.nueirat@student.aaup.edu>
2023-11-18 19:38:49 +0200

    Update next.config.js => add react Strict Mode

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode:true,
     // output: 'export',
     // distDir: 'dist',   =>   // Optional: Change the output directory `out` -> `dist`
     images: {
